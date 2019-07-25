2dec0422f050f2074bfdcf61e622e4f9ef8e5b1b
estrattonbailey <ericstrattonbailey@gmail.com>
2019-07-25 11:12:30 -0400

    fix: typo in json, add json tests

--- packages/json/index.js
+++ packages/json/index.js
@@ -1,16 +1,18 @@
+const error = require('http-errors')
+
 function parse (handler) {
   try {
     handler.event.body = JSON.parse(handler.event.body)
   } catch (e) {
-    throw createError(400, `The request body was malformed.`)
+    throw error(400, `The request body was malformed.`)
   }
 }
 
 function stringify (handler) {
   try {
-    handler.response.body = JSON.parse(handler.response.body)
+    handler.response.body = JSON.stringify(handler.response.body)
   } catch (e) {
-    throw createError(400, `The response body was malformed.`)
+    throw error(400, `The response body was malformed.`)
   }
 }
 
