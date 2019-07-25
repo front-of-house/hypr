752a6447f60ecfac4ec1dca5327562f0077fdead
estrattonbailey <ericstrattonbailey@gmail.com>
2019-07-25 11:37:58 -0400

    update: add config options for json

--- packages/json/index.js
+++ packages/json/index.js
@@ -1,18 +1,44 @@
 const error = require('http-errors')
 
-function parse (handler) {
-  try {
-    handler.event.body = JSON.parse(handler.event.body)
-  } catch (e) {
-    throw error(400, `The request body was malformed.`)
+function parse (options = {}) {
+  const {
+    type = 'application/json'
+  } = options
+
+  return handler => {
+    const ev = handler.event
+
+    if (ev.httpMethod === 'GET') return
+
+    if (ev.headers['content-type'] !== type) {
+      throw error(400, `Specified ${type} but request body was malformed`)
+    }
+
+    try {
+      ev.body = JSON.parse(ev.body)
+    } catch (e) {
+      throw error(400, `The request body was malformed.`)
+    }
   }
 }
 
-function stringify (handler) {
-  try {
-    handler.response.body = JSON.stringify(handler.response.body)
-  } catch (e) {
-    throw error(400, `The response body was malformed.`)
+function stringify (options = {}) {
+  const {
+    type = 'application/json'
+  } = options
+
+  return handler => {
+    const res = handler.response
+
+    try {
+      res.body = JSON.stringify(res.body)
+      res.headers = {
+        ...res.headers,
+        'content-type': type
+      }
+    } catch (e) {
+      throw error(400, `The response body was malformed.`)
+    }
   }
 }
 
