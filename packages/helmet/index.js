3a9af6fa04e568ec751fb57a2cfcd48d6e31399f
estrattonbailey <ericstrattonbailey@gmail.com>
2019-07-25 11:49:29 -0400

    feature: basic helmet middleware

---
+++ packages/helmet/index.js
@@ -0,0 +1,14 @@
+module.exports = function helmet (options = {}) {
+  return handler => {
+    handler.response.headers = {
+      ...handler.response.headers,
+      'content-type': 'application/json',
+      'x-dns-prefetch-control': 'off',
+      'x-frame-options': 'sameorigin',
+      'x-download-options': 'noopen',
+      'x-content-type-options': 'nosniff',
+      'x-xss-protection': '1; mode=block',
+      'strict-transport-security': 'max-age=5184000'
+    }
+  }
+}
