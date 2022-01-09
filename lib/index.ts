e61c3b97290d47503d6cc8be64f6ec525991dd86
estrattonbailey <ericstrattonbailey@gmail.com>
2022-01-09 11:55:00 -0600

    feat: export createHandler

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,1 @@
-export { stack, main, HttpError, Event, Context, LambdaResponse as Response } from './hypr'
+export { stack, main, createHandler, HttpError, Event, Context, LambdaResponse as Response } from './hypr'
